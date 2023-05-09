d62890f6cce798ef9bce97bbc0b87bbcd97ce49d
Eishalex2 <t.whitney.a@gmail.com>
2023-05-09 11:39:07 -0400

    feat: Add page load logic
    Next: UI add task logic
    
    Thoughts: Thinking about separating the pageLoad function into its own
    module, so having pageLoad and then the dom logic functions separate.

--- src/dom.js
+++ src/dom.js
@@ -2,8 +2,52 @@ import CreateProject from "./createProject";
 import CreateTask from "./createToDo";
 import addTaskToProject from "./addToDo";
 
-const projectList = document.getElementById('projects');
+const projectDisplay = document.getElementById('projects');
+const taskDisplay = document.getElementById('tasks');
 
-const defaultProject = CreateProject("Inbox");
+let currentProject;
 
-projectList.appendChild(defaultProject.name);
+function createProjectAdd() {
+  const newProjectBtn = document.createElement('button');
+  newProjectBtn.classList.add('new-project');
+  newProjectBtn.textContent = '+ New Project';
+  projectDisplay.appendChild(newProjectBtn);
+}
+
+function createTaskAdd() {
+  const newTaskBtn = document.createElement('button');
+  newTaskBtn.classList.add('new-task');
+  newTaskBtn.textContent = '+ Add Task';
+  taskDisplay.appendChild(newTaskBtn);
+}
+
+function projectsUI(project) {
+  const projectBtn = document.createElement('button');
+  projectBtn.classList.add('project');
+  projectBtn.textContent = project.name;
+  projectDisplay.appendChild(projectBtn);
+}
+
+function tasksUI(task) {
+  const taskBtn = document.createElement('button');
+  taskBtn.classList.add('task');
+  taskBtn.textContent = task.title;
+  taskDisplay.appendChild(taskBtn);
+}
+
+// function for showing tasks, switching projects. To remove children,
+// set textcontent to "";
+
+function pageLoad() {
+  // projects
+  const defaultProject = CreateProject('Inbox');
+  currentProject = defaultProject;
+  projectsUI(defaultProject);
+  createProjectAdd();
+
+  // tasks
+  defaultProject.taskList.forEach(task => tasksUI(task));
+  createTaskAdd();
+}
+
+export { pageLoad };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-import CreateProject from "./createProject";
-import CreateTask from "./createToDo";
-import addTaskToProject from "./addToDo";
+import { pageLoad } from "./dom";
 
-const projectDisplay = document.getElementById('projects');
-const taskDisplay = document.getElementById('tasks');
-
-const defaultProject = CreateProject("Inbox");
-
-const task0 = CreateTask('stuff');
-
-addTaskToProject(task0, defaultProject);
-
-const InboxBtn = document.createElement('button');
-InboxBtn.textContent = defaultProject.name;
-
-projectDisplay.appendChild(InboxBtn);
-
-InboxBtn.addEventListener('click', () => {
-  defaultProject.taskList.forEach(object => {
-    const taskBtn = document.createElement('button')
-    taskBtn.textContent = object.title;
-    taskDisplay.appendChild(taskBtn);
-  })
-
-})
+pageLoad();
